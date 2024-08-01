4f49f65decfcffa9979902c9f2010eb7c36b5e0e
Siburian <91644864+ApsaelSiburian@users.noreply.github.com>
2024-08-01 21:22:29 +0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Menggunakan 'react-dom/client'
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // Membuat root dengan createRoot
-root.render(<App />); // Menggunakan root.render untuk merender aplikasi
-
